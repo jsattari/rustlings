b761f33ed37f8180bc5d514a347a9d7b5fcdc615
John Sattari <jsattar3@gmail.com>
2024-09-11 17:40:05 -0700

    finished chp 1 exercises

---
+++ exercises/01_variables/variables1.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // TODO: Add the missing keyword.
+    // added let and typing
+    let x: i32 = 5;
+
+    println!("x has the value {x}");
+}
---
+++ exercises/01_variables/variables6.rs
@@ -0,0 +1,7 @@
+// TODO: Change the line below to fix the compiler error.
+// added variable type
+const NUMBER: i32 = 3;
+
+fn main() {
+    println!("Number: {NUMBER}");
+}
