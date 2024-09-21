5ae5562b173bf34a73c3989dcced214bf44afbce
John Sattari <jsattar3@gmail.com>
2024-09-20 20:43:25 -0700

    completed chp 6 problems

---
+++ exercises/06_move_semantics/move_semantics5.rs
@@ -0,0 +1,27 @@
+#![allow(clippy::ptr_arg)]
+
+// TODO: Fix the compiler errors without changing anything except adding or
+// removing references (the character `&`).
+
+// Shouldn't take ownership
+// added reference
+fn get_char(data: &String) -> char {
+    data.chars().last().unwrap()
+}
+
+// Should take ownership
+// removed reference
+fn string_uppercase(mut data: String) {
+    data = data.to_uppercase();
+
+    println!("{data}");
+}
+
+fn main() {
+    let data = "Rust is great!".to_string();
+
+    // added reference
+    get_char(&data);
+
+    string_uppercase(data);
+}
