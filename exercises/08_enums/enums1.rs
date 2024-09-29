50067683cb24a8e4acee9679efb9820b2ecd40de
John Sattari <jsattar3@gmail.com>
2024-09-28 20:30:26 -0700

    completed chp 8 probs

---
+++ exercises/08_enums/enums1.rs
@@ -0,0 +1,18 @@
+#[derive(Debug)]
+enum Message {
+    // TODO: Define a few types of messages as used below.
+    // added below patterns
+    Quit,
+    Move,
+    Echo,
+    ChangeColor,
+    Resize
+}
+
+fn main() {
+    println!("{:?}", Message::Resize);
+    println!("{:?}", Message::Move);
+    println!("{:?}", Message::Echo);
+    println!("{:?}", Message::ChangeColor);
+    println!("{:?}", Message::Quit);
+}
